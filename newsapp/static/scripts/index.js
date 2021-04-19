f01eac44b0c5dbfc8c14b74e780b7128ffe9f4d8
wanglingxiao <lingxiao.wang@ucdconnect.ie>
2021-04-19 21:56:15 +0800

    debugs

--- newsapp/static/scripts/index.js
+++ newsapp/static/scripts/index.js
@@ -21,9 +21,9 @@ $(document).ready(function (){
         $("#originalPrice").html(root2.childNodes[3].childNodes[3].childNodes[3].childNodes[0].innerHTML);
         $("#flowerNum").html(root2.childNodes[3].childNodes[3].childNodes[5].innerHTML+" in stock now")
         $("#flowerIntro").html(root2.childNodes[3].childNodes[3].childNodes[7].innerHTML)
-        $("#target_image1").attr("src",root2.childNodes[1].childNodes[1].childNodes[1].src);
+        $("#target_image1").attr("src",root2.childNodes[1].childNodes[1].childNodes[3].src);
         $("#target_image_bottom1").attr("src",root2.childNodes[1].childNodes[1].childNodes[3].src);
         $("#target_image2").attr("src",root2.childNodes[1].childNodes[1].childNodes[1].src);
-        $("#target_image_bottom2").attr("src",root2.childNodes[1].childNodes[1].childNodes[3].src);
+        $("#target_image_bottom2").attr("src",root2.childNodes[1].childNodes[1].childNodes[1].src);
     })
 })
